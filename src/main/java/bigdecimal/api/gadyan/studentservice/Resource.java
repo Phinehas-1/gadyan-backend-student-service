b992184acae2c8c5300581f4d9e2b862f31e3d33
Phinehas Enakireru <phinehasenakireru@gmail.com>
2022-11-10 13:26:14 +0100

    add [addStudent] feature -endpoint and service methods. (stage #1)

--- src/main/java/bigdecimal/api/gadyan/studentservice/Resource.java
+++ src/main/java/bigdecimal/api/gadyan/studentservice/Resource.java
@@ -1,10 +1,39 @@
 package bigdecimal.api.gadyan.studentservice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import bigdecimal.api.gadyan.studentservice.domain.Student;
 
 @RestController
 @RequestMapping("/api/v1")
 public class Resource {
-    
+    private ResponseEntity<? extends Object> response;
+
+    private final Service service;
+
+    public Resource(Service service) {
+        this.service = service;
+    }
+
+    @PostMapping("/addStudent")
+    public ResponseEntity<? extends Object> addStudent(@RequestBody Student student) {
+        response = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (student.getStudent_batch_id() == null || student.getStudent_name() == null
+                || student.getStudent_name().trim().isEmpty()) {
+            return response;
+        }
+        try {
+            service.addStudent(student);
+            response = new ResponseEntity<>(HttpStatus.CREATED);
+            return response;
+        } catch (Exception e) {
+            response = new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return response;
+    }
 }
--- src/main/java/bigdecimal/api/gadyan/studentservice/Service.java
+++ src/main/java/bigdecimal/api/gadyan/studentservice/Service.java
@@ -5,6 +5,7 @@ import javax.persistence.Id;
 
 import org.springframework.data.repository.CrudRepository;
 
+import bigdecimal.api.gadyan.studentservice.domain.Student;
 import lombok.Data;
 
 @org.springframework.stereotype.Service
@@ -13,6 +14,17 @@ public class Service {
 
     public Service(DAO repo) {
         this.repo = repo;
+    }
+
+    public void addStudent(Student student) throws EntityNotSavedException {
+        StudentEntity entity = new StudentEntity();
+        entity.setStudent_name(student.getStudent_name());
+        entity.setStudent_batch_id(student.getStudent_batch_id());
+        try {
+            repo.save(entity);
+        } catch (Exception e) {
+            throw new EntityNotSavedException(e);
+        }
     }
 }
 
@@ -23,6 +35,13 @@ class StudentEntity {
     private Long student_id;
     private String student_name;
     private Long student_batch_id;
+}
+
+class EntityNotSavedException extends Exception {
+    public EntityNotSavedException(Throwable t) {
+        super("This operation failed to save the entity.");
+        t.printStackTrace();
+    }
 }
 
 interface DAO extends CrudRepository<StudentEntity, Long>{  
